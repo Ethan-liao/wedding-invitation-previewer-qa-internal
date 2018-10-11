39cceaf25f2bdc9a86990edf31ea3ff6b83cbfc4
Ethan-liao <ethan-liao@outlook.com>
2018-10-11 00:21:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ name2.addEventListener('keyup',function(e){
 })
 
 
-let initial1 = name1.value[0];
-let initial2 = name2.value[0];
+let initial1 = name1.value;
+let initial2 = name2.value;
 
 initial.innerHTML = `${initial1} & ${initial2}`;
 
