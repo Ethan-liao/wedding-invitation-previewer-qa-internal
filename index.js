8788fa18580789a2463ea4ae5fd8c03eafac61bf
Ethan-liao <ethan-liao@outlook.com>
2018-10-11 01:05:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ let initial2;
  let imageContainer = document.querySelector('.previewContainer');
 
 casualBtn.addEventListener('click',function(e){
-  imageContainer.style.backgroundImage = url('https://i.pinimg.com/originals/df/16/21/df1621249c94b8dfbfa929dcd0f2df7f.png');
+  imageContainer.style.backgroundImage = "url('https://i.pinimg.com/originals/df/16/21/df1621249c94b8dfbfa929dcd0f2df7f.png')";
 })
 
 beachBtn.addEventListener('click',function(e){
-  imageContainer.style.backgroundImage = url('https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/beach-with-big-rock-ahead-vertical-bermuda-heather-kirk.jpg');
+  imageContainer.style.backgroundImage = "url('https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/beach-with-big-rock-ahead-vertical-bermuda-heather-kirk.jpg')";
 })
     // mlBtn,
 
